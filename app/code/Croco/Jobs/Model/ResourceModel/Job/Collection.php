ae8480a562a240baace8224b2295d0478a83537b
bahaa <ahmedbahaa.3@hotmail.com>
2020-08-26 01:07:48 +0200

    Bug: Jobs Count retrieved = 0

--- app/code/Croco/Jobs/Model/ResourceModel/Job/Collection.php
+++ app/code/Croco/Jobs/Model/ResourceModel/Job/Collection.php
@@ -18,4 +18,16 @@ class Collection extends AbstractCollection
         $this->_init('Croco\Jobs\Model\Job', 'Croco\Jobs\Model\ResourceModel\Job');
     }
 
+    public function addStatusFilter($job, $department){
+        $this->addFieldToSelect('*')
+            ->addFieldToFilter('status', $job->getEnableStatus())
+            ->join(
+                array('department' => $department->getResource()->getMainTable()),
+                'main_table.department_id = department.'.$department->getIdFieldName(),
+                array('department_name' => 'name')
+            );
+
+        return $this;
+    }
+
 }
